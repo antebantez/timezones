0cc835da7d1ee67d1b290381c251e56ff4c6027f
Seans99 <sean99@live.nl>
2022-01-18 13:03:52 +0100

    added separate css file for header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Container, Nav} from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+
+import classes from "/public/Header.module.css";
 
 
 
 export default function Header() {
   return (
-<Navbar bg="light" color='white' fixed="top" expand="lg">
+    <Navbar className={classes.nav} bg="light" color='white' fixed="top" expand="lg">
       <Container>
-<Navbar.Brand href="#home">Time-Zones</Navbar.Brand>
+        <Navbar.Brand href="#home">Time-Zones</Navbar.Brand>
       </Container>
       <Container>
-    
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
