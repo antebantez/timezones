d868f1ee41ed00535aecc2e5c8c014b8560f81ef
Seans99 <sean99@live.nl>
2022-01-18 14:29:25 +0100

    Added a card for cities page

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 import classes from "/public/Header.module.css";
 
-
-
 export default function Header() {
   return (
-    <Navbar className={classes.nav} bg="light" color='white' fixed="top" expand="lg">
+    <Navbar
+      className={classes.nav}
+      bg="light"
+      color="white"
+      fixed="top"
+      expand="lg"
+    >
       <Container>
         <Navbar.Brand href="#home">Time-Zones</Navbar.Brand>
       </Container>
       <Container>
-
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,5 +1,46 @@
+import classes from "/public/Cities.module.css";
+
+import { Card } from "react-bootstrap";
+
 export default function Cities() {
   return (
-    <h1>Cities</h1>
+    <div className={classes.card}>
+      <Card className={classes.citiesCard} style={{ width: "60rem" }} >
+        <Card.Body className={classes.cardBody}>
+          <div className={classes.citiesRow1}>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+          </div>
+          <div className={classes.citiesRow2}>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+          </div>
+          <div className={classes.citiesRow1}>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+          </div>
+          <div className={classes.citiesRow2}>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+          </div>
+          <div className={classes.citiesRow1}>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+          </div>
+          <div className={classes.citiesRow2}>
+            <Card.Text as="h5">"City name"</Card.Text>
+            <Card.Text as="h5">"City name"</Card.Text>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
