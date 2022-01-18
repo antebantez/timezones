19220437f5ca195d4c7af2470a04de36b6197fd1
Seans99 <sean99@live.nl>
2022-01-18 16:27:02 +0100

    Added useEffect to fetch data from cities.json

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,8 +1,18 @@
+import { useState, useEffect } from "react";
 import classes from "/public/Cities.module.css";
 
 import { Card } from "react-bootstrap";
 
 export default function Cities() {
+
+  const [city, setCity] = useState([]);
+
+  useEffect(async () => {
+    setCity(await (await fetch("./json/cities.json")).json());
+  }, []);
+
+  console.log(city);
+
   return (
     <div className={classes.card}>
       <Card className={classes.citiesCard} style={{ width: "60rem" }}>
