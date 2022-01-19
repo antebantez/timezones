66868aabb28d63bf5d35d2d09b6cc8628ff2ebb0
adamLundstedt <adamlundstedt@rocketmail.com>
2022-01-19 11:22:06 +0100

    working on footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Cities from "./pages/Cities";
 import CityPage from "./pages/CityPage";
 import MyTime from "./pages/MyTime";
 import MissingPage from "./pages/MissingPage";
+import Footer from "./Footer";
 
 export default function App() {
   return (
@@ -21,6 +22,7 @@ export default function App() {
           <Route path="/*" element={<MissingPage />} />
           <Route path="/cities/*" element={<MissingPage />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </main>
   );
---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+import classes from "/public/Footer.module.css";
+
+export default function Footer() {
+  return (
+  <Navbar bg="dark" variant="dark" fixed="bottom" expand="lg">
+  <Container>
+    <Navbar.Brand >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere eum ab aut? </Navbar.Brand>
+  </Container>
+</Navbar>);
+}
