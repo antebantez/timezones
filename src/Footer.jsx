7f58167acd4d228a1a6c9b41ee3280c408261b62
adamlundstedt <adamlundstedt@rocketmail.com>
2022-01-19 14:44:41 +0100

    Foot work

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Navbar, Container, Nav } from "react-bootstrap";
 
 import classes from "/public/Footer.module.css";
 
 export default function Footer() {
-  return (
-  <Navbar bg="dark" variant="dark" fixed="bottom" expand="lg">
-  <Container>
-    <Navbar.Brand >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere eum ab aut? </Navbar.Brand>
-  </Container>
-</Navbar>);
+  return <footer>
+    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. - john doe</h3>
+  </footer>
 }
