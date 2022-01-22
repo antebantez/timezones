610ce7f19d7a132f49ef004ca53c6253a5cee5d1
Sara Iqbal <sara.iqbal@hotmail.se>
2022-01-22 10:05:41 +0100

    Created and design missing page and designed the component "Card"

--- src/pages/MissingPage.jsx
+++ src/pages/MissingPage.jsx
@@ -1,5 +1,19 @@
+import { Card, Button, Row } from '../utilities/components-bootstrap';
+
 export default function MissingPage() {
-  return (
-    <h1>Missing Page</h1>
+  return (<>
+    <Card>
+      <Row>
+        <h1>404</h1>
+      </Row>
+      <Row>
+        <h2>That page doesn't exist!</h2>
+      </Row>
+      <Row>
+        {/* Function på kanpp kvar att skapa */}
+        <Button>Back to home page</Button>
+        </Row>
+    </Card>
+    </>
   );
 }
--- src/utilities/components-bootstrap.js
+++ src/utilities/components-bootstrap.js
@@ -2,5 +2,8 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-export { Container, Navbar, Nav, Col };
+export { Container, Navbar, Nav, Col, Row, Card, Button };
