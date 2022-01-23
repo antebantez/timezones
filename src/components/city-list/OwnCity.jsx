b49f02230df0e0ee0b05cfa855e6661b3b17871b
Nehcnud <nenaddjukin@gmail.com>
2022-01-23 16:45:06 +0100

    Jag har lagt till två cards till OwnCity sidan som ska vara delad i två. Det är bara att ändra den ena som är i botten

--- src/components/city-list/OwnCity.jsx
+++ src/components/city-list/OwnCity.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from "react";
-// import { Card } from "react-bootstrap";
-import Form from 'react-bootstrap/Form';
-import { Button } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form'
+import { Row, Card, Col, Button, Container } from "../../utilities/components-bootstrap";
+;
+
+
 
 // Ett försökt till att spara till i localStorage
 export default function OwnCity() {
@@ -13,22 +15,60 @@ export default function OwnCity() {
     setownCity, setZone
   };
 
+
+
+
+  function resetForm() {
+
+    changeFormValues({ ...emptyFormValues });
+
+  }
+
   return (
-    // user input plus knapp
-    <div className="input-field mb-3">
+    <Container>
+
+      <Row>
+        <Col>
+          <Card>
+            <div className="input-field mb-3">
 
 
-      <Form.Control
-        id='cityName' type="text" placeholder="City" value={localStorage.getItem('cityName')}
-      ></Form.Control>
+              <Form.Control
+                id='cityName' type="text" style={{ background: "rgba(196, 196, 196, 0.3)", border: "none" }} placeholder="City" value={localStorage.getItem('cityName')}
+              ></Form.Control>
 
-      <Form.Control
-        id='timeZone' type="text" placeholder="Time Zone" value={localStorage.getItem('timeZone')}
-      ></Form.Control>
+              <Form.Control
+                id='timeZone' type="text" style={{ background: "rgba(196, 196, 196, 0.3)", border: "none" }} placeholder="Time Zone" value={localStorage.getItem('timeZone')}
+              ></Form.Control>
 
-      {/* Ett försök till att spara user input på onClick */}
-      <Button onClick={clickEvent, setZone, setownCity} variant="primary"> Add to list</Button>
-    </div>
+              {/* Ett försök till att spara user input på onClick */}
+              <Button type="button" style={{ background: "rgba(196, 196, 196, 0.3)", border: "none" }} class="btn btn-primary btn-lg"> + Submit </Button>
+              <Button type="button" style={{ background: "rgba(196, 196, 196, 0.3)", border: "none" }} class="btn btn-primary btn-lg"> Clear </Button>
+            </div>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <div className="input-field mb-3">
+
+
+              <Form.Control
+                id='cityName' type="text" placeholder="City" value={localStorage.getItem('cityName')}
+              ></Form.Control>
+
+              <Form.Control
+                id='timeZone' type="text" placeholder="Time Zone" value={localStorage.getItem('timeZone')}
+              ></Form.Control>
+
+              {/* Ett försök till att spara user input på onClick */}
+              <Button onClick={clickEvent, setZone, setownCity} variant="primary"> Add to list</Button>
+            </div>
+          </Card>
+        </Col>
+
+      </Row>
+
+    </Container>
   )
 
 
