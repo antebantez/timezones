7ac43e7249c6bab1b70419bcf8f452b9c816ad14
adamLundstedt <adamlundstedt@rocketmail.com>
2022-01-25 00:46:52 +0100

    more quotes, added fader to quotes.

--- src/components/quotes.jsx
+++ src/components/quotes.jsx
@@ -1,21 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import QuotesData from '../../public/json/quotes.json'
 
+let quote = QuotesData[Math.floor(Math.random() * (QuotesData.length - 1))].quote;
 
+const toggleHidden = (setIsHidden) => {
+  setIsHidden(true);
+  setTimeout(function () {
+    quote = QuotesData[Math.floor(Math.random() * (QuotesData.length - 1))].quote;
+    setIsHidden(false);
+}, 2000);
+}
 
 export default function quotes() {
-  const [quote, setQuote] = useState();
+  const [isHidden, setIsHidden] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-       const randomIndex = Math.floor(Math.random() * QuotesData.length - 1);
-      setQuote(QuotesData[randomIndex].quote);
-    }, 20000);
+    const interval = setInterval(toggleHidden, 20000, setIsHidden);
   }, []);
 
   return (
     <footer>
-      <h3>{quote}</h3>
+      <h3 className={`quote ${isHidden ? 'hide' : ''}`}>{quote}</h3>
     </footer>
   )
 }
