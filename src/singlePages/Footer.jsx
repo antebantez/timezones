4fc932f36c0bfb384a2e30ca2b5e2d2e8de5309d
adamLundstedt <adamlundstedt@rocketmail.com>
2022-01-31 20:47:10 +0100

    changed footer.css to scss

--- src/singlePages/Footer.jsx
+++ src/singlePages/Footer.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Quotes from '../components/quotes'
 
-import classes from "/public/Footer.module.css";
-
 export default function Footer() {
   return <Quotes />
 }
