66d8bdaaf7f7a7921c8e0a4e986d9222f1b6d87c
Nehcnud <nenaddjukin@gmail.com>
2022-01-31 17:56:55 +0100

    mindre ändringar

--- src/components/city-list/OwnCity.jsx
+++ src/components/city-list/OwnCity.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React from "react";
 import { useState, useEffect } from "react";
-import useStates from '../../utilities/useStates'
-import Form from 'react-bootstrap/Form'
-import { Row,Card, Col, Button, Container } from "../../utilities/components-bootstrap";
-;
+import useStates from "../../utilities/useStates";
+import Form from "react-bootstrap/Form";
+import {
+  Row,
+  Card,
+  Col,
+  Button,
+  Container,
+} from "../../utilities/components-bootstrap";
 export default function OwnCity(props) {
   let { setAndSaveItems, stateList } = props;
-
 
   let [formValues, changeFormValues] = useStates({
     id: "5053",
     city: "",
     timezone: "",
     image: "none.",
-    imageBgr: "None"
+    imageBgr: "None",
   });
 
   function updateFormValue(event) {
@@ -39,94 +43,111 @@ export default function OwnCity(props) {
   let { city, timezone } = formValues;
   const clearForm = city.length > 0;
   return (
-    
-    
     <Container className="MyTime">
       <Row>
         <Col>
-          <Card className='p-3'>
-            <h3 className='FavoriteText'>Creat Your Own Time:</h3>
-    <div className="input-field mb-3">
-      <form onSubmit={submitForm}>
-        <Form.Group className="mb-3">
-                  <Form.Control 
-                    
-            type="text"
-            name="city"
-            value={city}
-            
-            onChange={updateFormValue}
-            placeholder="City"
-            required
-            minLength={2}
-           maxLength={10}
-          style={{ background: "rgba(190, 190, 190, 0.3)", border: "none" }}
-          />
-        </Form.Group>
-                <Form.Group className="mb-3" >
+          <Card className="p-3">
+            <h3 className="FavoriteText">Create Your Own Time:</h3>
+            <div className="input-field mb-3">
+              <form onSubmit={submitForm}>
+                <Form.Group className="mb-3">
+                  <Form.Control
+                    type="text"
+                    name="City"
+                    value={city}
+                    onChange={updateFormValue}
+                    placeholder="City"
+                    required
+                    minLength={2}
+                    style={{
+                      background: "rgba(190, 190, 190, 0.3)",
+                      border: "none",
+                    }}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
                   <Form.Label htmlFor="disabledSelect">Select UTC</Form.Label>
-          <Form.Select name="timezone" onChange={updateFormValue} style={{ background: "rgba(190, 190, 190, 0.3)", border: "none" }} required >
-                  <option value="" hidden  defaultValue >
-                    
-             Zone
-            </option>
-            <option value={14}>UTC+14:00</option>
-            <option value={13}>UTC+13:00</option>
-            <option value={12.75}>UTC+12:45</option>
-            <option value={12}>UTC+12:00</option>
-            <option value={11}>UTC+11:00</option>
-            <option value={10.5}>UTC+10:30</option>
-            <option value={10}>UTC+10:00</option>
-            <option value={9.5}>UTC+9:30</option>
-            <option value={9}>UTC+9:00</option>
-            <option value={8.75}>UTC+8:45</option>
-            <option value={8}>UTC+8:00</option>
-            <option value={7}>UTC+7:00</option>
-            <option value={6.5}>UTC+6:30</option>
-            <option value={6}>UTC+6:00</option>
-            <option value={5.75}>UTC+5:45</option>
-            <option value={5.5}>UTC+5:30</option>
-            <option value={5}>UTC+5:00</option>
-            <option value={4.5}>UTC+4:30</option>
-            <option value={4}>UTC+4:00</option>
-            <option value={3}>UTC+3:00</option>
-            <option value={2}>UTC+2:00</option>
-            <option value={1}>UTC+1:00</option>
-            <option value={0}>UTC+0:00</option>
-            <option value={-1}>UTC-1:00</option>
-            <option value={-2}>UTC-2:00</option>
-            <option value={-3}>UTC-3:00</option>
-            <option value={-3.5}>UTC-3:30</option>
-            <option value={-4}>UTC-4:00</option>
-            <option value={-5}>UTC-5:00</option>
-            <option value={-6}>UTC-6:00</option>
-            <option value={-7}>UTC-7:00</option>
-            <option value={-8}>UTC-8:00</option>
-            <option value={-9}>UTC-9:00</option>
-            <option value={-9.5}>UTC-9:30</option>
-            <option value={-10}>UTC-10:00</option>
-            <option value={-11}>UTC-11:00</option>
-            <option value={-12}>UTC-12:00</option>
-          </Form.Select>
-              </Form.Group>
-              
-        <Button type="submit" style={{ background: "rgba(196, 196, 196, 0.3)", border: "transparent" }} value="Add" className=" btn-primary btn-lg">
-          Submit{" "}
-        </Button>
-        {clearForm && (
-          <Button value="Add" style={{ background: "rgba(196, 196, 196, 0.3)", border: "auto" }} className="btn-primary btn-lg" onClick={clearForms} >
-            Clear{" "}
-          </Button>
-          )}
-        
-      </form>
-        </div>
+                  <Form.Select
+                    name="timezone"
+                    onChange={updateFormValue}
+                    style={{
+                      background: "rgba(190, 190, 190, 0.3)",
+                      border: "none",
+                    }}
+                    required
+                  >
+                    <option value="" hidden defaultValue id="ZoneColor">
+                      Zone
+                    </option>
+                    <option value={14}>UTC+14:00</option>
+                    <option value={13}>UTC+13:00</option>
+                    <option value={12.75}>UTC+12:45</option>
+                    <option value={12}>UTC+12:00</option>
+                    <option value={11}>UTC+11:00</option>
+                    <option value={10.5}>UTC+10:30</option>
+                    <option value={10}>UTC+10:00</option>
+                    <option value={9.5}>UTC+9:30</option>
+                    <option value={9}>UTC+9:00</option>
+                    <option value={8.75}>UTC+8:45</option>
+                    <option value={8}>UTC+8:00</option>
+                    <option value={7}>UTC+7:00</option>
+                    <option value={6.5}>UTC+6:30</option>
+                    <option value={6}>UTC+6:00</option>
+                    <option value={5.75}>UTC+5:45</option>
+                    <option value={5.5}>UTC+5:30</option>
+                    <option value={5}>UTC+5:00</option>
+                    <option value={4.5}>UTC+4:30</option>
+                    <option value={4}>UTC+4:00</option>
+                    <option value={3}>UTC+3:00</option>
+                    <option value={2}>UTC+2:00</option>
+                    <option value={1}>UTC+1:00</option>
+                    <option value={0}>UTC+0:00</option>
+                    <option value={-1}>UTC-1:00</option>
+                    <option value={-2}>UTC-2:00</option>
+                    <option value={-3}>UTC-3:00</option>
+                    <option value={-3.5}>UTC-3:30</option>
+                    <option value={-4}>UTC-4:00</option>
+                    <option value={-5}>UTC-5:00</option>
+                    <option value={-6}>UTC-6:00</option>
+                    <option value={-7}>UTC-7:00</option>
+                    <option value={-8}>UTC-8:00</option>
+                    <option value={-9}>UTC-9:00</option>
+                    <option value={-9.5}>UTC-9:30</option>
+                    <option value={-10}>UTC-10:00</option>
+                    <option value={-11}>UTC-11:00</option>
+                    <option value={-12}>UTC-12:00</option>
+                  </Form.Select>
+                </Form.Group>
+
+                <Button
+                  type="submit"
+                  style={{
+                    background: "rgba(196, 196, 196, 0.3)",
+                    border: "transparent",
+                  }}
+                  value="Add"
+                  className=" btn-primary btn-lg"
+                >
+                  Submit{" "}
+                </Button>
+                {clearForm && (
+                  <Button
+                    value="Add"
+                    style={{
+                      background: "rgba(196, 196, 196, 0.3)",
+                      border: "auto",
+                    }}
+                    className="btn-primary btn-lg"
+                    onClick={clearForms}
+                  >
+                    Clear{" "}
+                  </Button>
+                )}
+              </form>
+            </div>
           </Card>
-         </Col>
-      </Row > 
+        </Col>
+      </Row>
     </Container>
-   
   );
 }
-
-
--- src/pages/MyTime.jsx
+++ src/pages/MyTime.jsx
@@ -1,52 +1,42 @@
-import React from 'react';
-import { useState } from 'react';
-import store from '../utilities/localStore';
-import AddCity from '../components/city-list/AddCity';
-import FavoriteList from '../components/city-list/FavoriteList';
+import React from "react";
+import { useState } from "react";
+import store from "../utilities/localStore";
+import AddCity from "../components/city-list/AddCity";
+import FavoriteList from "../components/city-list/FavoriteList";
 //import OwnCity from '../components/city-list/OwnCity'
-import { Card, Col, Container, Form, Row } from 'react-bootstrap';
+import { Card, Col, Container, Form, Row } from "react-bootstrap";
 
 function MyTime() {
-
- 
- 
-  
   store.list = store.list || [];
   store.save();
-  const [stateList,setStateList] = useState(store.list);
+  const [stateList, setStateList] = useState(store.list);
   function setAndSaveItems(items) {
     setStateList(items);
     store.list = items;
     store.save();
   }
 
-
-    return (
-      <div className="favoriteSite">
-        
-        
-        
-        <Container className="MyTime mt-5">
-          <Row >
-            <Col>
-              <AddCity setAndSaveItems={setAndSaveItems} stateList={stateList} />
-            </Col>
-            <Col >
-              <Card className='p-3'>
-                <h3 className='FavoriteText'>Your Choices:</h3>
-                <div className="favoriteList">
-                 
-                  
-          <FavoriteList setAndSaveItems={setAndSaveItems} stateList={stateList} />
-                  
-          </div>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-
-      </div>
-      
-    );
+  return (
+    <div className="favoriteSite">
+      <Container className="MyTime mt-5">
+        <Row>
+          <Col xl={6} lg={6} md={12} sm={12} xs={12}>
+            <AddCity setAndSaveItems={setAndSaveItems} stateList={stateList} />
+          </Col>
+          <Col xl={6} lg={6} md={12} sm={12} xs={12}>
+            <Card className="p-3">
+              <h3 className="FavoriteText">Your Choices:</h3>
+              <div className="favoriteList">
+                <FavoriteList
+                  setAndSaveItems={setAndSaveItems}
+                  stateList={stateList}
+                />
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 export default MyTime;
