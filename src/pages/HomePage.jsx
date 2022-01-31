14c3d0b6aff153cb2a1def7138e6aff3d77e3abc
Anton Olsson <antonolsson.imp@gmail.com>
2022-01-31 14:37:12 +0100

    Small margin fix on homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,12 +9,12 @@ export default function HomePage() {
     <Container>
       <Card className="standardCard">
         <Row>
-          <Col xl={1} lg={1} md={1} sm={2} xs={2}></Col>
-          <Col xl={5} lg={5} md={5} sm={10} xs={10}>
+          <Col xl={1} lg={1} md={1} sm={2} xs={2} className="mb-2"></Col>
+          <Col xl={5} lg={5} md={5} sm={10} xs={10} className="mb-2">
             <AnalogClock />
           </Col>
-          <Col xl={1} lg={1} md={1} sm={2} xs={1}></Col>
-          <Col xl={5} lg={5} md={5} sm={10} xs={11}>
+          <Col xl={1} lg={1} md={1} sm={2} xs={1} className="mb-2"></Col>
+          <Col xl={5} lg={5} md={5} sm={10} xs={11} className="mb-2">
             <Calendar />
           </Col>
         </Row>
