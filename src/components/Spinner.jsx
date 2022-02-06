56e4842d453d653b1e2133f7e237a2346ebafa1d
adamLundstedt <adamlundstedt@rocketmail.com>
2022-02-06 09:46:10 +0100

    Added comments

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+/* This Spinner was my intent to use as a loading animation for diffrent elements. 
+It never made it to the final product.
+It was created only witch CSS.  */
+
 export default function Spinner() {
   return (
     <div class="spinner">
